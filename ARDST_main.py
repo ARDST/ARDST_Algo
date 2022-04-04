44daa7f5948218d93a5dc53639227c9060ef4f57
ARDST <92865956+ARDST@users.noreply.github.com>
2022-04-04 15:48:43 +0800

    Update ARDST_main.py

--- ARDST_main.py
+++ ARDST_main.py
@@ -25,7 +25,7 @@ class Flatten(nn.Module):
 def main():
     if FLAGS.dataset == "Mnist":
         print("This dataset is Mnist")
-        data = read_data_sets(FLAGS.data_path, FLAGS.percentage,FLAGS.training_type,FLAGS.testing_type)
+        data = read_data_sets(FLAGS.data_path, FLAGS.percentage, FLAGS.training_type, FLAGS.testing_type, FLAGS.attack_eps)
 
     vtree = Vtree.read(FLAGS.vtree)
 
@@ -78,76 +78,70 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset",type=str,default="Mnist",help="There are four types of data sets in total"
-                                                                 "Mnist"
-                                                                 "FashionMnist"
-                                                                 "Cifar10"
-                                                                 "TrafficSigns")
+    
+    parser.add_argument("--dataset", type = str, 
+                        default = "Mnist", 
+                        choices = ["Mnist", "FashionMnist", "TrafficSigns"], 
+                        help = "There are four types of data sets in total" )
 
-    parser.add_argument("--training_type",type=str,default="FGSM",help="There are baseline to learn adversarial examples"
-                                                                       "FGSM"
-                                                                       "DeepFool"
-                                                                       "C&W-target"
-                                                                       "BIM"
-                                                                       "PGD")
+    parser.add_argument("--training_type", type=str, 
+                        default = "FGSM", 
+                        choices = ["FGSM", "DeepFool", "BIM", "PGD"], 
+                        help="There are baseline to learn adversarial examples")
 
-    parser.add_argument("--testing_type",type=str,default="FGSM",help="There are four types of attacks in total"
-                                                                      "FGSM"
-                                                                      "DeepFool"
-                                                                      "C&W-target"
-                                                                      "BIM"
-                                                                      "PGD")
+    parser.add_argument("--testing_type", type = str, 
+                        default = "FGSM", 
+                        choices = ["FGSM", "DeepFool", "BIM", "PGD"], 
+                        help = "There are four types of attacks in total")
 
-    parser.add_argument("--data_path", type=str,   default="./dataset/Mnist/", help="Directory for the stored input data."
-                                                                                           "Mnist Path:       ./dataset/Mnist/"
-                                                                                           "Fashion Path:     ./dataset/FashionMnist/"
-                                                                                           "Cifar10 Path:     ./dataset/Cifar10"
-                                                                                           "TrafficSign Path: ./dataset/TrafficSigns")
+    parser.add_argument("--attack_eps", type = float, 
+                        default = 0.01, 
+                        choices = [0.01, 0.1, 0.3, 0.5], 
+                        help = "There are about attack model eps values")
+    
+    parser.add_argument("--data_path", type = str,  
+                        default = "./dataset/Mnist/", 
+                        choices = ["./dataset/Mnist/", "./dataset/FashionMnist/", "./dataset/TrafficSigns"], 
+                        help = "Directory for the stored input data.")
 
-    parser.add_argument("--num_classes", type=int, default=10, help="Number of classes in the classification task.")
+    parser.add_argument("--num_classes", type = int, 
+                        default = 10, 
+                        choices = [10, 10, 43], 
+                        help = "Number of classes in the classification task.")
 
-    parser.add_argument("--vtree", type=str,       default="Struct/DLTreeStr_784.vtree", help="Path for vtree.")
+    parser.add_argument("--vtree", type = str,
+                        default = "Struct/DLTreeStr_784.vtree", 
+                        help = "Path for vtree.")
 
-    parser.add_argument(
-        "--circuit",
-        type=str,
-        default="",
-        help="[Optional] File path for the saved logistic circuit to load. "
-        "Note this circuit has to be based on the same vtree as provided in --vtree.",
-    )
+    parser.add_argument("--circuit", type = str,
+                        default = "",
+                        help = "[Optional] File path for the saved logistic circuit to load. "
+                        "Note this circuit has to be based on the same vtree as provided in --vtree.", )
 
-    parser.add_argument(
-        "--num_structure_learning_iterations",
-        type=int,
-        default=200,
-        help="[Optional] Num of iterations for structure learning. Its default value is 5000.",
-    )
+    parser.add_argument("--num_structure_learning_iterations", type = int,
+                        default = 200,
+                        help = "[Optional] Num of iterations for structure learning. Its default value is 5000.",)
 
-    parser.add_argument(
-        "--num_parameter_learning_iterations",
-        type=int,
-        default=15,
-        help="[Optional] Number of iterations for parameter learning after the structure is changed."
-        "Its default value is 15.",
-    )
+    parser.add_argument("--num_parameter_learning_iterations", type = int,
+                        default = 15,
+                        help = "[Optional] Number of iterations for parameter learning after the structure is changed."
+                        "Its default value is 15.",)
 
-    parser.add_argument("--depth", type=int, default=2, help="[Optional] The depth of every split. Its default value is 2.")
+    parser.add_argument("--depth", type = int, 
+                        default = 2, 
+                        help = "[Optional] The depth of every split. Its default value is 2.")
 
-    parser.add_argument(
-        "--num_splits",
-        type=int,
-        default=5,
-        help="[Optional] The number of splits in one iteration of structure learning." "It default value is 3.",
-    )
+    parser.add_argument("--num_splits", type = int,
+                        default = 5,
+                        help = "[Optional] The number of splits in one iteration of structure learning." "It default value is 3.", )
 
-    parser.add_argument(
-        "--percentage",
-        type=float,
-        default=0.001,
-        help="[Optional] The percentage of the training dataset that will be used. " "Its default value is 100%%. 1.0",
-    )
+    parser.add_argument("--percentage", type = float,
+                        default = 0.001,
+                        help="[Optional] The percentage of the training dataset that will be used. " "Its default value is 100%%. 1.0",)
 
-    parser.add_argument("--save_path", type=str, default="", help="[Optional] File path to save the best-performing circuit.")
+    parser.add_argument("--save_path", type = str, 
+                        default = "", 
+                        help = "[Optional] File path to save the best-performing circuit.")
 
     FLAGS = parser.parse_args()
     if FLAGS.num_classes == 2:
