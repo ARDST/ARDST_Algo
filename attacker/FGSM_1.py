bfd50845f6c8d3ce39cc0a9e99a320480ac21061
ARDST <92865956+ARDST@users.noreply.github.com>
2022-04-04 15:53:08 +0800

    Update FGSM_1.py

--- attacker/FGSM_1.py
+++ attacker/FGSM_1.py
@@ -8,15 +8,15 @@ import attacker.Model.MNIST_Model as MNIST_Model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 '''This is the datasets'''
-Mnist_train_data = datasets.MNIST(root='dataset/Mnist/',
-                                  train=True,
-                                  download=True,
-                                  transform=transforms.ToTensor())
+Mnist_train_data = datasets.MNIST(root = 'dataset/Mnist/',
+                                  train = True,
+                                  download = True,
+                                  transform = transforms.ToTensor())
 
-Mnist_test_data=datasets.MNIST(root='dataset/Mnist/',
-                               train=False,
-                               download=True,
-                               transform=transforms.ToTensor())
+Mnist_test_data = datasets.MNIST(root = 'dataset/Mnist/',
+                                 train = False,
+                                 download = True,
+                                 transform = transforms.ToTensor())
 
 Mnist_train_loader = torch.utils.data.DataLoader(Mnist_train_data,batch_size=1)
 Mnist_test_loader = torch.utils.data.DataLoader(Mnist_test_data,batch_size=1)
@@ -26,12 +26,12 @@ Mnist_target_model = MNIST_Model.MNIST_Model()#.cuda()
 Mnist_target_model.load_state_dict(torch.load("attacker/pt_file/mnist.pt",map_location='cpu'))
 Mnist_target_model.eval()
 
-def FGSM_Mnist_train(numbers):
+def FGSM_Mnist_train(numbers, eps_values):
     image = []
     label = []
     label_one = []
     number = numbers
-    atk = torchattacks.FGSM(Mnist_target_model, eps=0.5)
+    atk = torchattacks.FGSM(Mnist_target_model, eps = eps_values)
     for i,(images,labels) in enumerate(Mnist_train_loader):
         if i == number:
             break
@@ -46,14 +46,14 @@ def FGSM_Mnist_train(numbers):
     image = np.array(image)
     label = label
     label_one = label_one
-    return image,label,label_one
+    return image, label, label_one
 
-def FGSM_Mnist_test(numbers):
+def FGSM_Mnist_test(numbers, eps_values):
     image = []
     label = []
     label_one = []
     number = numbers
-    atk = torchattacks.FGSM(Mnist_target_model, eps=0.5)
+    atk = torchattacks.FGSM(Mnist_target_model, eps = eps_values)
     for i,(images,labels) in enumerate(Mnist_test_loader):
         if i == number:
             break
@@ -68,5 +68,5 @@ def FGSM_Mnist_test(numbers):
     image = np.array(image)
     label = label
     label_one = label_one
-    return image,label,label_one
+    return image, label, label_one
 
